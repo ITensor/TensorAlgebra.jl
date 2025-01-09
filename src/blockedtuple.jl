05fc957cd97853a4c129a7f6829e61d47cd1ed61
Olivier Gauthé <ogauthe@flatironinstitute.org>
2025-01-09 17:29:07 -0500

    define AbstractBlockTuple

--- src/blockedtuple.jl
+++ src/blockedtuple.jl
@@ -3,12 +3,14 @@
 
 using BlockArrays: Block, BlockArrays, BlockIndexRange, BlockRange, blockedrange
 
-struct BlockedTuple{Blocklengths,Flat}
+abstract type AbstractBlockTuple end
+
+struct BlockedTuple{BlockLengths,Flat} <: AbstractBlockTuple
   flat::Flat
 
-  function BlockedTuple{Blocklengths}(flat::Tuple) where {Blocklengths}
-    length(flat) != sum(Blocklengths) && throw(DimensionMismatch("Invalid total length"))
-    return new{Blocklengths,typeof(flat)}(flat)
+  function BlockedTuple{BlockLengths}(flat::Tuple) where {BlockLengths}
+    length(flat) != sum(BlockLengths) && throw(DimensionMismatch("Invalid total length"))
+    return new{BlockLengths,typeof(flat)}(flat)
   end
 end
 
@@ -19,28 +21,29 @@ BlockedTuple(bt::BlockedTuple) = bt
 # Base interface
 Base.Tuple(bt::BlockedTuple) = bt.flat
 
-Base.axes(bt::BlockedTuple) = (blockedrange([blocklengths(bt)...]),)
+Base.axes(bt::AbstractBlockTuple) = (blockedrange([blocklengths(bt)...]),)
 
-Base.broadcastable(bt::BlockedTuple) = bt
-struct BlockedTupleBroadcastStyle{Blocklengths} <: Broadcast.BroadcastStyle end
-function Base.BroadcastStyle(::Type{<:BlockedTuple{Blocklengths}}) where {Blocklengths}
-  return BlockedTupleBroadcastStyle{Blocklengths}()
+Base.broadcastable(bt::AbstractBlockTuple) = bt
+struct BlockedTupleBroadcastStyle{BlockLengths} <: Broadcast.BroadcastStyle end
+function Base.BroadcastStyle(type::Type{<:AbstractBlockTuple})
+  return BlockedTupleBroadcastStyle{blocklengths(type)}()
 end
+
+# BroadcastStyle is not called for two identical styles
 function Base.BroadcastStyle(::BlockedTupleBroadcastStyle, ::BlockedTupleBroadcastStyle)
   throw(DimensionMismatch("Incompatible blocks"))
 end
-# BroadcastStyle is not called for two identical styles
 function Base.copy(
-  bc::Broadcast.Broadcasted{BlockedTupleBroadcastStyle{Blocklengths}}
-) where {Blocklengths}
-  return BlockedTuple{Blocklengths}(bc.f.((Tuple.(bc.args))...))
+  bc::Broadcast.Broadcasted{BlockedTupleBroadcastStyle{BlockLengths}}
+) where {BlockLengths}
+  return BlockedTuple{BlockLengths}(bc.f.((Tuple.(bc.args))...))
 end
 
 Base.copy(bt::BlockedTuple) = BlockedTuple{blocklengths(bt)}(copy.(Tuple(bt)))
 
 Base.deepcopy(bt::BlockedTuple) = BlockedTuple{blocklengths(bt)}(deepcopy.(Tuple(bt)))
 
-Base.firstindex(::BlockedTuple) = 1
+Base.firstindex(::AbstractBlockTuple) = 1
 
 Base.getindex(bt::BlockedTuple, i::Integer) = Tuple(bt)[i]
 Base.getindex(bt::BlockedTuple, r::AbstractUnitRange) = Tuple(bt)[r]
@@ -51,26 +54,29 @@ Base.getindex(bt::BlockedTuple, bi::BlockIndexRange{1}) = bt[Block(bi)][only(bi.
 Base.iterate(bt::BlockedTuple) = iterate(Tuple(bt))
 Base.iterate(bt::BlockedTuple, i::Int) = iterate(Tuple(bt), i)
 
-Base.lastindex(bt::BlockedTuple) = length(bt)
+Base.lastindex(bt::AbstractBlockTuple) = length(bt)
 
 Base.length(bt::BlockedTuple) = length(Tuple(bt))
 
 Base.map(f, bt::BlockedTuple) = BlockedTuple{blocklengths(bt)}(map(f, Tuple(bt)))
 
 # BlockArrays interface
-function BlockArrays.blockfirsts(bt::BlockedTuple)
+function BlockArrays.blockfirsts(bt::AbstractBlockTuple)
   return (0, cumsum(blocklengths(bt)[begin:(end - 1)])...) .+ 1
 end
 
-function BlockArrays.blocklasts(bt::BlockedTuple)
+function BlockArrays.blocklasts(bt::AbstractBlockTuple)
   return cumsum(blocklengths(bt)[begin:end])
 end
 
-BlockArrays.blocklength(bt::BlockedTuple) = length(blocklengths(bt))
+BlockArrays.blocklength(bt::AbstractBlockTuple) = length(blocklengths(bt))
 
-BlockArrays.blocklengths(::BlockedTuple{Blocklengths}) where {Blocklengths} = Blocklengths
+BlockArrays.blocklengths(bt::AbstractBlockTuple) = blocklengths(typeof(bt))
+function BlockArrays.blocklengths(::Type{<:BlockedTuple{BlockLengths}}) where {BlockLengths}
+  return BlockLengths
+end
 
-function BlockArrays.blocks(bt::BlockedTuple)
+function BlockArrays.blocks(bt::AbstractBlockTuple)
   bf = blockfirsts(bt)
   bl = blocklasts(bt)
   return ntuple(i -> Tuple(bt)[bf[i]:bl[i]], blocklength(bt))
