0c3ea2f5fb6fba29b450b126b036f4f04de0f8a6
Olivier Gauthé <ogauthe@flatironinstitute.org>
2025-02-14 11:48:21 -0500

    fix labels

--- test/test_gradedunitrangesext_contract.jl
+++ test/test_gradedunitrangesext_contract.jl
@@ -22,11 +22,11 @@ const elts = (Float32, Float64, Complex{Float32}, Complex{Float64})
   d = gradedrange([U1(0) => 2, U1(1) => 3])
   a1 = randn_blockdiagonal(elt, (d, d, dual(d), dual(d)))
   a2 = randn_blockdiagonal(elt, (d, d, dual(d), dual(d)))
-  a_dest, dimnames_dest = contract(a1, (1, -1, 2, -3), a2, (2, -1, 1, -2))
+  a_dest, dimnames_dest = contract(a1, (1, -1, 2, -2), a2, (2, -3, 1, -4))
   a1_dense = densearray(a1)
   a2_dense = densearray(a2)
   a_dest_dense, dimnames_dest_dense = contract(
-    a1_dense, (1, -1, 2, -3), a2_dense, (2, -1, 1, -2)
+    a1_dense, (1, -1, 2, -2), a2_dense, (2, -3, 1, -4)
   )
   @test dimnames_dest == dimnames_dest_dense
   @test a_dest ≈ a_dest_dense
