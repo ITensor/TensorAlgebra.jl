721db2a7922ab8eb576f483e42c3e4541b259396
Lukas Devos <ldevos98@gmail.com>
2025-03-12 15:31:22 -0400

    Fix exports test

--- test/test_exports.jl
+++ test/test_exports.jl
@@ -1,6 +1,8 @@
 using TensorAlgebra: TensorAlgebra
 using Test: @test, @testset
 @testset "Test exports" begin
-  exports = [:TensorAlgebra, :contract, :contract!]
+  exports = [
+    :TensorAlgebra, :contract, :contract!, :eig, :eigvals, :lq, :qr, :svd, :svdvals
+  ]
   @test issetequal(names(TensorAlgebra), exports)
 end
