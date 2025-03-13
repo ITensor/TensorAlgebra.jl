d132d0bb7ef58ac6aea90b4b294b1034b420942d
mtfishman <mfishman@flatironinstitute.org>
2025-03-13 10:30:59 -0400

    Define random_unitary constructor

--- ext/TensorAlgebraGradedUnitRangesExt/TensorAlgebraGradedUnitRangesExt.jl
+++ ext/TensorAlgebraGradedUnitRangesExt/TensorAlgebraGradedUnitRangesExt.jl
@@ -1,8 +1,29 @@
 module TensorAlgebraGradedUnitRangesExt
-using GradedUnitRanges: AbstractGradedUnitRange, tensor_product
-using TensorAlgebra: TensorAlgebra
+
+using GradedUnitRanges: AbstractGradedUnitRange, dual, tensor_product
+using GradedUnitRanges.BlockArrays: Block, blocklengths, blocksize
+using Random: AbstractRNG
+using TensorAlgebra: TensorAlgebra, random_unitary
 
 function TensorAlgebra.:⊗(a1::AbstractGradedUnitRange, a2::AbstractGradedUnitRange)
   return tensor_product(a1, a2)
 end
+
+function TensorAlgebra.dual(a::AbstractGradedUnitRange)
+  return dual(a)
+end
+
+function TensorAlgebra.random_unitary(
+  rng::AbstractRNG,
+  elt::Type,
+  ax::Tuple{AbstractGradedUnitRange},
+)
+  a = zeros(elt, dual.(ax)..., ax...)
+  # TODO: Define `blockdiagindices`.
+  for i in 1:minimum(blocksize(a))
+    a[Block(i, i)] = random_unitary(rng, elt, Int(blocklengths(only(ax))[i]))
+  end
+  return a
+end
+
 end
---
+++ src/random_unitary.jl
@@ -0,0 +1,69 @@
+# Version of `sign` that returns one
+# if `x == 0`.
+function nonzero_sign(x)
+  iszero(x) && return one(x)
+  return sign(x)
+end
+
+using LinearAlgebra: LinearAlgebra, Diagonal, diag
+function qr_positive(M::AbstractMatrix)
+  Q, R = LinearAlgebra.qr(M)
+  Q′ = typeof(R)(Q)
+  signs = nonzero_sign.(diag(R))
+  Q′ = Q′ * Diagonal(signs)
+  R = Diagonal(conj.(signs)) * R
+  return Q′, R
+end
+
+using Random: Random, AbstractRNG
+
+dual(x) = x
+
+function random_unitary(
+  rng::AbstractRNG,
+  elt::Type,
+  ax::Tuple{AbstractUnitRange,Vararg{AbstractUnitRange}},
+)
+  ax_fused = ⊗(ax...)
+  a_fused = random_unitary(rng, elt, ax_fused)
+  return splitdims(a_fused, dual.(ax), ax)
+end
+
+# Copy of `Base.to_dim`:
+# https://github.com/JuliaLang/julia/blob/1431bec1bcd205f181ca2a3f1c314247b64076df/base/array.jl#L439-L440
+to_dim(d::Integer) = d
+to_dim(d::Base.OneTo) = last(d)
+
+# Matrix version.
+function random_unitary(
+  rng::AbstractRNG,
+  elt::Type,
+  ax::Tuple{AbstractUnitRange},
+)
+  return random_unitary(rng, elt, map(to_dim, ax))
+end
+
+function random_unitary(rng::AbstractRNG, elt::Type, dims::Tuple{Integer})
+  Q, _ = qr_positive(randn(rng, elt, (dims..., dims...)))
+  return Q
+end
+
+# Canonicalizing other kinds of inputs.
+function random_unitary(rng::AbstractRNG, elt::Type, dims::Tuple{Vararg{Union{AbstractUnitRange,Integer}}})
+  return random_unitary(Random.default_rng(), elt, map(to_axis, dims))
+end
+function random_unitary(elt::Type, dims::Tuple{Vararg{Union{AbstractUnitRange,Integer}}})
+  return random_unitary(Random.default_rng(), elt, dims)
+end
+function random_unitary(rng::AbstractRNG, elt::Type, dims::Union{AbstractUnitRange,Integer}...)
+  return random_unitary(rng, elt, dims)
+end
+function random_unitary(elt::Type, dims::Union{AbstractUnitRange,Integer}...)
+  return random_unitary(Random.default_rng(), elt, dims)
+end
+function random_unitary(rng::AbstractRNG, dims::Union{AbstractUnitRange,Integer}...)
+  return random_unitary(rng, Float64, dims)
+end
+function random_unitary(dims::Union{AbstractUnitRange,Integer}...)
+  return random_unitary(Random.default_rng(), Float64, dims)
+end
