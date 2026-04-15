d37aebf4134d8671f75b8214c01924219770aeed
mtfishman <mfishman@flatironinstitute.org>
2026-04-15 19:58:46 -0400

    Make export test version-aware for public names

--- test/test_exports.jl
+++ test/test_exports.jl
@@ -6,7 +6,6 @@ using Test: @test, @testset
         :TensorAlgebra,
         :contract,
         :contract!,
-        :contractopadd!,
         :eigen,
         :eigvals,
         :factorize,
@@ -14,7 +13,6 @@ using Test: @test, @testset
         :left_orth,
         :left_polar,
         :lq,
-        :matricizeop,
         :orth,
         :polar,
         :qr,
@@ -24,6 +22,10 @@ using Test: @test, @testset
         :svd,
         :svdvals,
     ]
+    # `public` (Julia 1.11+) adds names to `names()`; include them on 1.11+.
+    if VERSION >= v"1.11.0-DEV.469"
+        append!(exports, [:contractopadd!, :matricizeop])
+    end
     @test issetequal(names(TensorAlgebra), exports)
 
     exports = [
