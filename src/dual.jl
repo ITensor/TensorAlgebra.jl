589988cc3a3d225db003c05c1f10fe98e1bea83c
Matthew Fishman <mfishman@flatironinstitute.org>
2026-07-24 09:25:55 -0400

    Restore See also lines in dual/isdual docstrings

--- src/dual.jl
+++ src/dual.jl
@@ -3,6 +3,8 @@
 
 Returns `true` or `false` depending on if the axis `a` is dual. Falls back to `false`
 for `AbstractUnitRange`.
+
+See also [`dual`](@ref).
 """
 function isdual end
 isdual(::AbstractUnitRange) = false
@@ -12,6 +14,8 @@ isdual(::AbstractUnitRange) = false
 
 Returns the dual of the axis `a`. Falls back to returning `a` unchanged for
 `AbstractUnitRange`.
+
+See also [`isdual`](@ref).
 """
 function dual end
 dual(a::AbstractUnitRange) = a
