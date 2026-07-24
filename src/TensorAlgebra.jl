cae3515bcce6e14de506b1443f5cf3fcbcada586
Matthew Fishman <mfishman@flatironinstitute.org>
2026-07-24 09:28:45 -0400

    Export dual and isdual

--- src/TensorAlgebra.jl
+++ src/TensorAlgebra.jl
@@ -1,7 +1,7 @@
 module TensorAlgebra
 
-export contract, contract!, eig_full, eig_trunc, eig_vals, eigh_full, eigh_trunc,
-    eigh_vals, gram_eigh_full, gram_eigh_full_with_pinv, invsqrth_safe, left_null,
+export contract, contract!, dual, eig_full, eig_trunc, eig_vals, eigh_full, eigh_trunc,
+    eigh_vals, gram_eigh_full, gram_eigh_full_with_pinv, invsqrth_safe, isdual, left_null,
     left_orth, left_polar, lq_compact, lq_full, project_hermitian, qr_compact,
     qr_full, right_null, right_orth, right_polar, sqrth_invsqrth_safe, sqrth_safe,
     svd_compact, svd_full, svd_trunc, svd_vals
@@ -9,7 +9,7 @@ export contract, contract!, eig_full, eig_trunc, eig_vals, eigh_full, eigh_trunc
 if VERSION >= v"1.11.0-DEV.469"
     eval(
         Meta.parse(
-            "public biperm, bipartition, cat_similar, concatenate, concatenate!, ContractAlgorithm, contractopadd!, data, datatype, directsum, dual, flattenlinear, isdual, label_type, matricizeopperm, permutedims, permutedims!, scalar, similar_map, TensorOperationsAlgorithm, to_range, tr, tryflattenlinear, ungrade, zero!, scale!, permuteddims, PermutedDims"
+            "public biperm, bipartition, cat_similar, concatenate, concatenate!, ContractAlgorithm, contractopadd!, data, datatype, directsum, flattenlinear, label_type, matricizeopperm, permutedims, permutedims!, scalar, similar_map, TensorOperationsAlgorithm, to_range, tr, tryflattenlinear, ungrade, zero!, scale!, permuteddims, PermutedDims"
         )
     )
 end
